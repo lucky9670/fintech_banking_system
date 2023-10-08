09278b486409409f146b48a82498119ccd0da32d
Ritesh Singh <riteshbharti262903@gmail.com>
2023-10-08 13:42:39 +0530

    Fixing UI Issue

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('super_distibuter', views.superDistibuter, name = "super_distibuter"),
-    path('login', views.login, name = 'login')
+    path('login', views.login, name = 'login'),
+    path('register', views.registration, name = 'register'),
 ]
 # if settings.DEBUG:
 #     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
